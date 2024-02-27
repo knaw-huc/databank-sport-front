f199f9470411e8c2ff4fe7c3604117a6358f9c97
Rob Zeeman <rob.zeeman@di.huc.knaw.nl>
2024-02-27 19:39:44 +0100

    New browser base plus facets

--- src/components/facets.tsx
+++ src/components/facets.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {FreeTextFacet, ListFacet, ISendCandidate} from 'browser-base-react';
+import {FreeTextFacet, ListFacet, FilteredListFacet, ISendCandidate} from 'browser-base-react';
 
 export default function Facets({sendCandidateHandler}: { sendCandidateHandler: ISendCandidate }) {
     return <>
@@ -7,18 +7,21 @@ export default function Facets({sendCandidateHandler}: { sendCandidateHandler: I
         <ListFacet parentCallback={sendCandidateHandler}
                    name="Sport"
                    field="sports.sport"
-                   url="http://localhost:5000/facet"/>
-        <ListFacet parentCallback={sendCandidateHandler}
+                   url="http://localhost:5000/facet"
+        flex={false}/>
+        <FilteredListFacet parentCallback={sendCandidateHandler}
                    name="Plaats"
                    field="plaats"
-                   url="http://localhost:5000/facet"/>
+                   url="http://localhost:5000/filter-facet"/>
         <ListFacet parentCallback={sendCandidateHandler}
                    name="Provincie"
                    field="provincie"
-                   url="http://localhost:5000/facet"/>
+                   url="http://localhost:5000/facet"
+        flex={true}/>
         <ListFacet parentCallback={sendCandidateHandler}
                    name="Levensbeschouwing"
                    field="levensbeschouwing"
-                   url="http://localhost:5000/facet"/>
+                   url="http://localhost:5000/facet"
+        flex={false}/>
     </>;
 }
